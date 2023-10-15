9aaa579b777c8566d65bfddbd532113cbdf05e78
scoronosar <s.scoronosar.tj@gmail.com>
2023-10-15 16:17:29 +0500

    hw 2.3

---
+++ hw 2.3/hw 2.3/script.js
@@ -0,0 +1,73 @@
+let password = "hateyou";
+let requestPassword = prompt("Введите пароль");
+
+if (requestPassword === "password") {
+    alert("Пароль введен верно")
+}else{
+    alert("Пароль введен неправильно")
+}
+
+let c = 8
+
+if (c > 0 && c < 10) {
+    console.log("Верно")
+}else{
+    console.log("Неверно")
+};
+
+let d = 123;
+let e = 341;
+
+if (d > 100 || e > 100) {
+    console.log("верно")
+}else{
+    console.log("неверно")
+};
+
+let a = "2";
+let b = "3";
+
+alert(Number(a) + Number(b));
+
+let mouthNumber = 12;
+
+switch(mouthNumber){
+    case 1:
+        console.log("зима");
+        break;
+    case 12:
+        console.log("зима");
+        break;
+    case 2:
+        console.log("зима");
+        break;
+    case 3:
+        console.log("весна");
+        break;
+    case 4:
+        console.log("весна");
+        break;
+    case 5:
+        console.log("весна");
+        break;
+    case 6:
+        console.log("лето");
+        break;
+    case 7:
+        console.log("лето");
+        break;
+    case 8:
+        console.log("лето");
+        break;
+    case 9:
+        console.log("осень");
+        break;
+    case 10:
+        console.log("осень");
+        break;
+    case 11:
+        console.log("осень");
+        break;
+    default:
+        console.log("указано неверное число месяца")
+}
